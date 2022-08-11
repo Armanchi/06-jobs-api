9fb47805647fa74431a220ee6809ed4f14db5364
Armanchi <armanijohnson0104@gmail.com>
2022-08-11 08:33:46 -0500

    answers for week9

---
+++ routes/jobs.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+
+const {getAllJobs, getJob, createJob, updateJob, deleteJob} = require('../controllers/jobs')
+const { route } = require('./auth')
+
+router.route('/').post(createJob).get(getAllJobs)
+router.route('/:id').post(getJob).delete(deleteJob).patch(updateJob)
+
+
+module.exports = router 
